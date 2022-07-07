493fadd84ef4969bd70ea9d4d719c9536a1083e0
Thais Cristine <this.cristine06@gmail.com>
2022-07-07 16:22:49 +0300

    adding types dependencies

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
-import {Home} from './src/pages/Home';
+import { StatusBar } from 'react-native';
+import { Home } from './src/pages/Home';
 
 export default function App() {
   return (
